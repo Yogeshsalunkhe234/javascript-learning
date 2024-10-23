49edeee482fc146ee28f7e5fbf6334cebc4fa96e
Yogeshsalunkhe234 <salunkheyogesh234@gmail.com>
2024-10-23 12:31:24 +0530

    Variable Assignment in for one
    First assignments variables in js

---
+++ 01-variableAssignment.js
@@ -0,0 +1,15 @@
+ city ="Satara";
+console.log("city value :", city);
+
+friend="Akash";
+console.log("Best Friend:" ,friend);
+
+country="India";
+console.log("Country :",country);
+
+School="SGM Karad";
+console.log("School Name :", School);
+
+
+college="SGM Karad";
+console.log("college Name :", college);
---
+++ 02-variableAssignment.js
@@ -0,0 +1,38 @@
+myFullName="Yogesh Prashant Salunkhe";
+console.log("myFullName :",myFullName);
+
+myFullNameFirstandLastName="Yogesh Salunkhe";
+console.log("myFullName (First and Last Name) :",myFullNameFirstandLastName);
+
+firstname="Yogesh";
+console.log("First Name :",firstname);
+
+middelname="Prashant";
+console.log("Middele Name :",middelname);
+
+lastname="Salunkhe";
+console.log("Last Name :",lastname);
+
+language="I lOVE Only Js";
+console.log("Language :",language);
+
+firstname="Yogesh";
+console.log("First Name :",firstname);
+
+lastname="Salunkhe";
+console.log("Last Name :",lastname);
+
+age="23"
+console.log("Age:",age);
+
+address="Patan";
+console.log("Address:",address);
+
+pincode="415209";
+console.log("Pincode:",pincode);
+
+Gender="Male";
+console.log("Gender:",Gender);
+
+isMarried="False";
+console.log("isMarried:",isMarried);
